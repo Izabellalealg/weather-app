3cdc92fd4ff8f2709e143e2bd8af1794d3b73643
Izabella Leal Goncalves <izabellalgoncalves@gmail.com>
2022-01-12 14:30:53 -0300

    added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d580e6c1aa2d191b7d677dcaa33f3840";
-let city = "São Paulo";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "d580e6c1aa2d191b7d677dcaa33f3840";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
