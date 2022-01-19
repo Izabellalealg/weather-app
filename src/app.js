5b0369890991c7a4a032da8effdd1891f0d54fa5
Izabella Leal Goncalves <izabellalgoncalves@gmail.com>
2022-01-19 20:24:57 -0300

    added JS template for the forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Wed", "Thur", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+                </div>
+                <img src="http://openweathermap.org/img/wn/01n@2x.png" alt="" width="52">
+                <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span> 
+                <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+              </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
@@ -89,4 +112,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+displayForecast();
 search("New York");
